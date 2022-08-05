7ae8e36c9acc84be2fc4290d55893d5e89152191
bethanyshoun <bethanyshoun@gmail.com>
2022-08-04 21:17:07 -0400

    began laying out framework for user-routes and User.js files

---
+++ utils/helpers.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET all posts for dashboard
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
