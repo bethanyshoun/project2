ae3ddb434024afdfb0e62537e9068355e5fb000b
bethanyshoun <bethanyshoun@gmail.com>
2022-08-09 21:31:07 -0400

    remove extra seed files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, heart } = require('../models');
+const { Post, User, Comment, Heart } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
